ecdd317a0b27b4b9b4465deb8fd162d4fe8739d1
dkovalskyi <dkovalskyi@playtika>
2017-05-26 10:34:52 +0300

    Added lstm model

---
+++ src/main/java/deep/RestrictedBoltzmannMachine.java
@@ -0,0 +1,66 @@
+package deep;
+
+import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RBM;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+/**
+ * Created by dkovalskyi on 30.03.2017.
+ */
+public class RestrictedBoltzmannMachine {
+    public static void main(String[] args) throws IOException {
+
+        final int numberOfRows = 28;
+        final int numberOfColumns = 28;
+        int seed = 123;
+        int numberOfIterations = 1;
+        MnistDataSetIterator iterator = new MnistDataSetIterator(100, MnistDataFetcher.NUM_EXAMPLES, true);
+
+
+        System.out.println("Build model....");
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(seed)
+                .iterations(numberOfIterations)
+                .optimizationAlgo(OptimizationAlgorithm.LINE_GRADIENT_DESCENT)
+                .list()
+                .layer(0, new RBM.Builder().nIn(numberOfRows * numberOfColumns).nOut(1000).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(1, new RBM.Builder().nIn(1000).nOut(500).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(2, new RBM.Builder().nIn(500).nOut(250).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(3, new RBM.Builder().nIn(250).nOut(100).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(4, new RBM.Builder().nIn(100).nOut(30).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build()) //encoding stops
+                .layer(5, new RBM.Builder().nIn(30).nOut(100).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build()) //decoding starts
+                .layer(6, new RBM.Builder().nIn(100).nOut(250).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(7, new RBM.Builder().nIn(250).nOut(500).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(8, new RBM.Builder().nIn(500).nOut(1000).lossFunction(LossFunctions.LossFunction.KL_DIVERGENCE).build())
+                .layer(9, new OutputLayer.Builder(LossFunctions.LossFunction.MSE).activation(Activation.SIGMOID).nIn(1000).nOut(numberOfRows * numberOfColumns).build())
+                .pretrain(true).backprop(true)
+                .build();
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        model.setListeners(Collections.singletonList(new ScoreIterationListener()));
+        System.out.println("Train model");
+        while (iterator.hasNext()) {
+            DataSet dataSet = iterator.next();
+            model.fit(new DataSet(dataSet.getFeatureMatrix(), dataSet.getFeatureMatrix()));
+        }
+
+        File modelFile = new File("savedModel");
+        ModelSerializer.writeModel(model, modelFile, true);
+
+    }
+}
